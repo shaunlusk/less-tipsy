27e64e3e87ff890aaedea452a71821e914ded44e
Shaun Lusk <shaun.lusk@gmail.com>
2020-08-22 14:16:33 -0500

    Modal component

---
+++ src/components/modal/modal-true-false-selection.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import './modal-true-false-selection.scss';
+
+export interface ITrueFalseSelectionModalProps {
+  handleClose(response: boolean): void;
+  show?: boolean;
+  acceptText: string;
+  rejectText: string;
+  title:string;
+}
+
+export class TrueFalseSelectionModal extends React.Component<ITrueFalseSelectionModalProps, any> {
+  private showHideClassName(): string {
+    return this.props.show ? 'modal display-block' : 'modal display-none';
+  }
+
+  public render() {
+    return <div className={this.showHideClassName()}>
+        <div className="modal-main">
+          <div className="modal-title">{this.props.title}</div>
+          <div className="modal-content">{this.props.children}</div>
+          <div className="modal-buttons">
+            <button className="modal-buttons-reject" onClick={() => this.props.handleClose(false)}>{this.props.rejectText}</button>
+            <button className="modal-buttons-accept" onClick={() => this.props.handleClose(true)}>{this.props.acceptText}</button>
+          </div>
+        </div>
+      </div>;
+  }
+}
