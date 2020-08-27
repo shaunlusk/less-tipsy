2021e77a41085b9a9b0ec16efc6e652f62c27b05
Shaun Lusk <shaun.lusk@gmail.com>
2020-08-26 19:56:23 -0500

    Barebones History panel

--- src/components/history-panel/history-panel.tsx
+++ src/components/history-panel/history-panel.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { DateDisplay } from '../date-display/date-display';
+import { NumberDisplay } from '../number-display/number-display';
+import './history-panel.scss';
 
 export interface IHistoryPanelSession {
   unitsConsumed: number;
@@ -19,12 +22,21 @@ class HistoryPanel extends React.Component<IHistoryPanelProps, any> {
     return <div>
         <h3>History</h3>
         <div className="history-panel-sessions">
-        {this.props.sessions.map(session => (
-          <div key={'session-' + (idx++)}>
-            <span>{session.date.toString()}</span>
-            <span>{session.unitsConsumed}</span>
-          </div>
-        ))}
+          <div>Date</div>
+          <div>Units</div>
+          <div>Session Max</div>
+          <div>Rolling Weekly</div>
+          <div>Weekly Max</div>
+        {this.props.sessions.map(session => {
+          
+          return <React.Fragment key={'session-' + (idx++)}>
+            <div><DateDisplay datetime={session.date} format="YYYY-MM-DD"></DateDisplay></div> 
+            <div><NumberDisplay number={session.unitsConsumed} decimalPlaces={1}></NumberDisplay></div>
+            <div><NumberDisplay number={session.sessionMax} decimalPlaces={1}></NumberDisplay></div>
+            <div><NumberDisplay number={session.rollingWeekly} decimalPlaces={1}></NumberDisplay></div>
+            <div><NumberDisplay number={session.weeklyMax} decimalPlaces={1}></NumberDisplay></div>
+          </React.Fragment>
+        })}
         </div>
         <div className="history-panel-buttons"><button onClick={this.props.deleteHistory}>Delete History</button></div>
       </div>
