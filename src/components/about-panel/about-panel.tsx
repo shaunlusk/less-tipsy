8085c2478635944e4bb1e12ec011e70df3fe2030
Shaun <shaun.lusk@gmail.com>
2020-10-20 21:03:50 -0500

    Small fixes

--- src/components/about-panel/about-panel.tsx
+++ src/components/about-panel/about-panel.tsx
@@ -5,13 +5,19 @@ export interface IAboutPanelProps {
 }
 
 export const AboutPanel: FunctionComponent<IAboutPanelProps> = ({viewedAboutPanel}) => {
+    window.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth'
+    });
     viewedAboutPanel();
     return <div>
         <h3>About</h3>
         <div>
             <p><strong>Alcohol Units</strong></p>
-            <p>This application uses Acohol Units to track alcohol consumption.  An alcohol unit is defined as 10ml or 8g of pure alcohol; this definition is provided by the National Health Service (UK):
-            https://www.nhs.uk/live-well/alcohol-support/calculating-alcohol-units/. </p>
+            <p>This application uses Acohol Units to track alcohol consumption.  An alcohol unit is defined as 10ml or 8g of pure alcohol; this definition is provided by the <span>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.nhs.uk/live-well/alcohol-support/calculating-alcohol-units/" title="Alcohol Units - NHS">National Health Service (UK)</a></span>.
+            </p>
             <p>As such, the defaults for alcohol consumption in this app are set to amount recommended by the NHS: a maximum of 14 units per week.  You may changes these settings, but you do so at your own risk.</p>
             <p>Also note that an alcohol unit is roughly (but not precisely!) equivalent to 1/2 of a U.S. "standard" drink.</p>
             <p><strong>Data Persistence</strong></p>
@@ -21,5 +27,9 @@ export const AboutPanel: FunctionComponent<IAboutPanelProps> = ({viewedAboutPane
                 (.csv format) from the History tab and import in the History tab of the target browser/device.</p>
             <p>While this isn't as convenient as cloud storage, it allows you to own the data, and prevents selling, theft, or misuse of the data.</p>
         </div>
+        <p><strong>Credits</strong></p>
+        <p>
+            Icon made by <a target="_blank" rel="noopener noreferrer" href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a target="_blank" rel="noopener noreferrer" href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        </p>
     </div>;
 }
